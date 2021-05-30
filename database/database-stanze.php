bbc848c054342fe578600bf22d223b2ff5112778
AlfonsoBuononato <alfonso_buononato@hotmail.it>
2021-05-30 22:45:31 +0200

    esercizio

--- database/database-stanze.php
+++ database/database-stanze.php
@@ -12,12 +12,34 @@ if($_GET === ""){
     $id = $_GET["id"];
 } */
 
+if($id){
+    "SELECT * FROM `STANZE` WHERE `id` = '$id'";
+
+    $stmt = $conn->prepare( "SELECT * FROM `STANZE` WHERE `id` = ?");
+    $stmt->bind_param('s', $id);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    if($result && $result->num_rows > 0 ){
+        $dato = $result->fetch_assoc();
+    }
+}
+
+
+$conn->close();
+
+
+
+
+
+/* 
 $sql = "SELECT * FROM `STANZE` WHERE `id` = '$id'";
 $result = $conn->query($sql);
 
 if($result && $result->num_rows > 0 ){
     $dato = $result->fetch_assoc();
-}
+} */
 
-$conn->close();
 ?>
